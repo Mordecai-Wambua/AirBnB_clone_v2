04b5ab229dd59b0c5932d217c00992da59bb1290
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-04-16 22:54:36 +0300

    task 02 attempt

--- console.py
+++ console.py
@@ -2,6 +2,7 @@
 """ Console Module """
 import cmd
 import sys
+import shlex
 from models.base_model import BaseModel
 from models.__init__ import storage
 from models.user import User
@@ -73,7 +74,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] =='}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,18 +114,55 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    def implementer(self, args):
+        """Implement the required syntax for the key-value pairs."""
+        syntax = {}
+        for arg in args[1:]:
+            if "=" in arg:
+                division = arg.split('=', 1)
+                k = division[0]
+                v = division[1]
+                if v[0] == v[-1] == '"':
+                    v = shlex.split(v)[0].replace('_', ' ')
+                else:
+                    try:
+                        v = int(v)
+                    except ValueError:
+                        try:
+                            v = float(v)
+                        except ValueError:
+                            continue
+                syntax[k] = v
+        return syntax
+
     def do_create(self, args):
-        """ Create an object of any class"""
+        """Performs object creation."""
         if not args:
             print("** class name missing **")
             return
-        elif args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
+
+        try:
+            args = shlex.split(args)
+            if args[0] in self.classes:
+                new_instance = eval(args[0])()
+                for i in args[1:]:
+                    try:
+                        key, value = i.split("=")
+                        if hasattr(new_instance, key):
+                            value = value.replace("_", " ")
+                            try:
+                                value = eval(value)
+                            except:
+                                pass
+                            setattr(new_instance, key, value)
+                    except ValueError:
+                        pass
+                new_instance.save()
+                print(new_instance.id)
+            else:
+                print("** class doesn't exist **")
+        except Exception as e:
+            print("** Error:", e)
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +310,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +318,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,47 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
+"""Defines the BaseModel class."""
+import models
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """A base class for all hbnb models"""
+    """Define all common attributes/methods for other classes."""
+
     def __init__(self, *args, **kwargs):
-        """Instantiates a new model"""
-        if not kwargs:
-            from models import storage
-            self.id = str(uuid.uuid4())
+        """Initialize attributes.
+
+        Args:
+            *args: unused
+            *kwargs (dict): attribures
+        """
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k in ['created_at', 'updated_at']:
+                        setattr(self, k, datetime.fromisoformat(v))
+                    else:
+                        setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4().hex)
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
-
-    def __str__(self):
-        """Returns a string representation of the instance"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+            models.storage.new(self)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
-        from models import storage
+        """Update the attribute update_at with the current datetime."""
         self.updated_at = datetime.now()
-        storage.save()
+        models.storage.save()
 
     def to_dict(self):
-        """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """Return a dictionary containing all keys/values."""
+        new = self.__dict__.copy()
+        new['__class__'] = self.__class__.__name__
+        new['created_at'] = self.created_at.isoformat()
+        new['updated_at'] = self.updated_at.isoformat()
+        return new
+
+    def __str__(self):
+        """Return string representation of the class."""
+        return ("[{}] ({}) {}".format(self.__class__.__name__,
+                self.id, self.__dict__))
