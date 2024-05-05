79cdddd67645772e6e51b12fa769625b05453f5f
livingstoneamisi <livingstoneamisi00@gmail.com>
2024-05-05 03:46:23 -0700

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
-# Installs Nginx if not installed
-#+ Creates folders /data/web_static/shared
-#+	and /adta/releases/test if not exists
-#+ /data/web_static/current linked to
-#+ /data/web_static/releases/test/
-#+ Creates an /data/web_static/releases/test/index.html
-#+ Configures Nginx to serve /data/web_static/current/ 
-#+	to hbnb_static
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
+# script that sets up web servers for the deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "Welcome to The_Masterminds home" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
+sudo ufw allow 'Nginx HTTP'
+
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
