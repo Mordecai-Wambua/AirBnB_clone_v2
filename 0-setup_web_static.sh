45fbc44bc2ee9d1c0a7737727b3e1ac749631155
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-06 17:19:23 +0300

    fixes

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,30 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
-
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data/
-
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
-=======
 #sets up your web servers for the deployment of web_static.
 
 sudo apt-get -y update
@@ -36,8 +10,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 echo "Hello World" > /data/web_static/releases/test/index.html
 
-sudo chown -R ubuntu /data/
-sudo chgrp -R ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 new_config=\
 "server {
@@ -49,7 +22,7 @@ new_config=\
         server_name _;
         add_header X-Served-By \$hostname;
 
-	location /hbnb_static {
+	location /hbnb_static/ {
         alias /data/web_static/current;
         index index.html index.htm;
 	}
@@ -66,7 +39,5 @@ new_config=\
 "
 echo "Ceci n'est pas une page" > /var/www/html/404.html
 echo "$new_config" > /etc/nginx/sites-available/default
-
->>>>>>> cad2f8870189f452856cbf2ffe1f5e3ee3cb985d
 
 sudo service nginx restart
