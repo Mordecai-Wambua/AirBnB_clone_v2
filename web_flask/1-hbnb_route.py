618d245287406a05b510402236fa609e049597da
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-16 22:24:30 +0300

    task 01

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -11,5 +11,11 @@ def hello():
     return "Hello HBNB!"
 
 
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB for the route /hbnb."""
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
