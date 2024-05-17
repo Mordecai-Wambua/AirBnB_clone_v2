2fa2d19aa3bfc1e1ad44f2b4389f848110346b8f
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-17 13:39:37 +0300

    task 03

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display Hello HBNB for the route /."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB for the route /hbnb."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def replace(text):
+    """Display C followed by text variable for route /c/text."""
+    x = text.replace("_", " ")
+    return f"C {x}"
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def defaults(text=None):
+    if text is None:
+        text="is cool"
+    """Display Python followed by text variable for route /python/<text>."""
+    x = text.replace("_", " ")
+    return f"Python {x}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
