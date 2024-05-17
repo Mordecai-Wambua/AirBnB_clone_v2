c0fe8c519289c84c9bf70175d1f6dc0dfac9750e
Mordecai-Wambua <mordecaingumbau@gmail.com>
2024-05-17 13:25:41 +0300

    task 02

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a Flask web application."""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """Display Hello HBNB for the route /."""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Display HBNB for the route /hbnb."""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def replace(text):
+    """Display C followed bny text variable for route /c/text."""
+    x = text.replace("_", " ")
+    return f"C {x}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
